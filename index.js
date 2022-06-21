0fa66bd80fa1fd0a9738473532b5da3780539132
Bishal Pahari <bishal.pahari53@gmail.com>
2022-06-21 14:05:05 +0545

    added extra classes in button and extra switch cases so that drum work with capital letter input

--- index.js
+++ index.js
@@ -16,63 +16,66 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 document.addEventListener("keypress", function(event) {
   makeSound(event.key)
-    buttonAnimation(event.key);
+  buttonAnimation(event.key);
 
 });
 
+//Sound
 function makeSound(key) {
   switch (key) {
     case "w":
+    case "W":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
     case "a":
+    case "A":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-
-    case "s":
+    case "S":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
 
     case "d":
+    case "D":
       var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
 
     case "j":
+    case "J":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
     case "k":
+    case "K":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
 
     case "l":
+    case "L":
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
 
     default:
-      console.log(buttonInnerHTML);
+      console.log("Pressed wrong key");
 
   }
 }
 
-function buttonAnimation(currentKey)
-{
-  var activeButton = document.querySelector("."+currentKey);
+//clicked animation
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
   activeButton.classList.add("pressed");
-  setTimeout(function(){
+  setTimeout(function() {
     activeButton.classList.remove("pressed");
-  },50);
+  }, 50);
 }
